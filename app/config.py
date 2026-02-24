797a4a0c2af3e3f3debd33f433c88120cdf0e47f
prakash.prasad <infymee@gmail.com>
2026-02-24 16:32:41 +0545

    settings updated for deployment

--- app/config.py
+++ app/config.py
@@ -1,10 +1,44 @@
-from pydantic_settings import BaseSettings
+from pydantic import computed_field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    sqlalchemy_string: str = "postgresql://prakash:adminadmin@localhost:5432/tutorialdb"
-    api_key:str = "70504d0ef78832dac8bcc465b976033b07261975ec5936a1ea45ac2b5deed372"
-    secret_key:str = "41998da4dcf4c423442d8fb6bbd67f3021f8cbae27ee701ce0e63014f90bc07e"
-    access_token_expires_days:int = 30 
-    algorithm:str = "HS256"
+    db_user:str 
+    db_password:str
+    db_host:str 
+    database:str 
+    token_api_key:str 
+    auth_secret_key:str 
+
+    @computed_field
+    @property
+    def sqlalchemy_string(self)->str:
+        return f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}:5432/{self.database}"
+    
+    @computed_field
+    @property
+    def api_key(self)->str:
+        return self.token_api_key
+    
+    @computed_field
+    @property
+    def secret_key(self)->str:
+        return self.auth_secret_key
+    
+    @computed_field
+    @property
+    def access_token_expires_day(self)->int:
+        return 30
+
+    @computed_field
+    @property
+    def algorithm(self)->str:
+        return "HS256"
+    
+
+    model_config = SettingsConfigDict(env_file=".env")
+    
+
+    
+
 settings = Settings()
 
